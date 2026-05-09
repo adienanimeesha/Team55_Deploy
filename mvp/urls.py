d6c0ca37c42573f3c38f4693ea45817b8bd9d87d
neaurellia <catherineaurellia@gmail.com>
2026-05-09 20:56:26 +1000

    edited script to read files in data folder

--- mvp/urls.py
+++ mvp/urls.py
@@ -19,9 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< Updated upstream
     path('', include('maps.urls')),
-=======
-    path('', include('navigation.urls')),
->>>>>>> Stashed changes
 ]
