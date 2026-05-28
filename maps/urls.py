6ba636d8ba05ec2e28b3cebc72f3325b458ca771
adienanimeesha <adiena.nimeesha@gmail.com>
2026-05-28 22:28:50 +1000

    fixed ux issues, made onboarding as the first page, skip onboarding directly to map for 'skip fow now'

--- maps/urls.py
+++ maps/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", RedirectView.as_view(pattern_name="onboarding", permanent=False)),
     path("building/<str:building_id>/", views.building_detail, name="building_detail"),
     path("reminders/", views.reminders, name="reminders"),
     path("onboarding/", views.onboarding, name="onboarding"),
+    path("map/", views.home, name="home"),
 ]
